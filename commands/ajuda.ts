db648e8af639151bc37bdb4f0036f89e08e79673
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-05-03 03:26:20 -0300

    ajdua now messages the channel and pings helpers

--- commands/ajuda.ts
+++ commands/ajuda.ts
@@ -1,5 +1,7 @@
-import { Message } from 'discord.js';
+import { Message, TextChannel } from 'discord.js';
 import { chatCommand, Command } from '../controllers/CommandController';
+import ConfigService from '../services/ConfigService';
+import Discord from 'discord.js';
 
 @chatCommand()
 class Ajuda implements Command {
@@ -9,9 +11,19 @@ class Ajuda implements Command {
 	usage = 'ajuda';
 
 	async exec(message: Message, args: string[]) {
+		// Send the help response to the user
 		let reply =
 			':cold_sweat:   Já estou chamando a equipe pra te ajudar! se possível, fique online na nossa sala "Ajuda / Suporte"';
+		await message.channel.send(reply);
 
-		message.channel.send(reply);
+		let guild = message.guild!;
+
+		// Get the config for this guild
+		let config = await ConfigService.getConfig(guild.id);
+
+		// Get the helpChannel, cast it to TextChannel.
+		let helpChannel = guild.channels.cache.get(config.helpChannelId) as TextChannel;
+
+		helpChannel.send(`Ajuda solicitada por: <@${message.member?.id}> <@&${config.helperRoleId}>\nMensagem: ${message.url}`);
 	}
 }
